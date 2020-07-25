c4ea458a5af2df1afcce64f7603a5efbc905c617
Maksym Leichenko <ky61k105@gmail.com>
2020-07-25 17:13:35 +0300

    25.07

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ Solved:
 16.07: 
 - [x] [easy] [string] [14. Longest Common Prefix](https://leetcode.com/problems/longest-common-prefix/)
 - [x] [easy] [string] [20. Valid Parentheses](https://leetcode.com/problems/valid-parentheses/)
+
+25.07: 
+- [x] [easy] [string] [58. Length of Last Word](https://leetcode.com/problems/length-of-last-word/)
---
+++ solutions/easy_string_58_length_of_last_word/index.js
@@ -0,0 +1,30 @@
+/**
+ https://leetcode.com/problems/length-of-last-word/
+
+ 58. Length of Last Word
+
+ Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word (last word means the last appearing word if we loop from left to right) in the string.
+
+ If the last word does not exist, return 0.
+
+ Note: A word is defined as a maximal substring consisting of non-space characters only.
+
+ Example:
+
+ Input: "Hello World"
+ Output: 5
+ */
+
+
+/**
+ * @param {string} str
+ * @return {number}
+ */
+const lengthOfLastWord = (str) => {
+  const strTrimmed = str.trim();
+  const index = strTrimmed.lastIndexOf(' ');
+  if (index === -1) {
+    return strTrimmed.length;
+  }
+  return strTrimmed.length - index - 1;
+};
